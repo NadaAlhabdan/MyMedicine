a4bccc7b964afd1edf5804059f8f7538a81b4d48
fayzah <ffowz110@hotmail.com>
2023-12-19 16:33:52 +0300

    Initial Commit

---
+++ zz/zzApp.swift
@@ -0,0 +1,17 @@
+//
+//  zzApp.swift
+//  zz
+//
+//  Created by Faizah Almalki on 06/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct zzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
